41611898d50fe8ec6d59389834479053857fdb60
tomasvr <tomas.rietbergen@gmail.com>
2021-12-13 17:27:14 +0100

    disable some logging on rf2o

--- src/rf2o_laser_odometry_ros2/src/CLaserOdometry2D.cpp
+++ src/rf2o_laser_odometry_ros2/src/CLaserOdometry2D.cpp
@@ -251,8 +251,8 @@ bool CLaserOdometry2D::odometryCalculation(const sensor_msgs::msg::LaserScan& sc
 
   auto m_runtime = get_clock()->now() - start;
 
-  RCLCPP_INFO(get_logger(), "[rf2o] execution time (ms): %f",
-                m_runtime.seconds()*double(1000));
+  // RCLCPP_INFO(get_logger(), "[rf2o] execution time (ms): %f",
+  //               m_runtime.seconds()*double(1000));
 
   //Update poses
   PoseUpdate();
@@ -934,10 +934,10 @@ void CLaserOdometry2D::PoseUpdate()
   kai_loc_old_(1) = -kai_abs_(0)*std::sin(phi) + kai_abs_(1)*std::cos(phi);
   kai_loc_old_(2) =  kai_abs_(2);
 
-  RCLCPP_INFO(get_logger(), "[rf2o] LASERodom = [%f %f %f]",
-                laser_pose_.translation()(0),
-                laser_pose_.translation()(1),
-                rf2o::getYaw(laser_pose_.rotation()));
+  // RCLCPP_INFO(get_logger(), "[rf2o] LASERodom = [%f %f %f]",
+  //               laser_pose_.translation()(0),
+  //               laser_pose_.translation()(1),
+  //               rf2o::getYaw(laser_pose_.rotation()));
 
   //Compose Transformations
   robot_pose_ = laser_pose_ * laser_pose_on_robot_inv_;
