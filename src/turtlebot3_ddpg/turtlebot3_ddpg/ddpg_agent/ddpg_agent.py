ffa75fe3d29d330d1b648c4da3b49480b08dd1d1
tomasvr <tomas.rietbergen@gmail.com>
2021-12-14 14:28:54 +0100

    try ddpg_1

--- src/turtlebot3_ddpg/turtlebot3_ddpg/ddpg_agent/ddpg_agent.py
+++ src/turtlebot3_ddpg/turtlebot3_ddpg/ddpg_agent/ddpg_agent.py
@@ -111,8 +111,8 @@ class DDPGAgent(Node):
             'src/turtlebot3_ddpg/model')
 
         # Specify which model and episode to load from models_directory or Change to False for new session
-        self.load_session = 'ddpg_9'  # example: 'ddpg_0'
-        self.load_episode = 3800 if self.load_session else 0
+        self.load_session = 'ddpg_1'  # example: 'ddpg_0'
+        self.load_episode = 700 if self.load_session else 0
 
         # Specify whether model is being trained or only evaluated
         self.trainig = False
